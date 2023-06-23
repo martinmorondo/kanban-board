9cb47e801e99f022eb34b4b5af94e4195bd6226d
Martín Morondo <martinmorondo@gmail.com>
2023-06-23 01:02:03 -0300

    add new features

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+      id: 1,
+      title: 'Por hacer',
+      tasks: [
+        { id: 1, title: 'Tarea 1' },
+        { id: 2, title: 'Tarea 2' },
+        // ...
+      ],
+    },
+    {
+      id: 2,
+      title: 'En progreso',
+      tasks: [
+        { id: 3, title: 'Tarea 3' },
+        { id: 4, title: 'Tarea 4' },
+        // ...
+      ],
+    },
+    {
+      id: 3,
+      title: 'Completadas',
+      tasks: [
+        { id: 5, title: 'Tarea 5' },
+        { id: 6, title: 'Tarea 6' },
+        // ...
+      ],
+    },
+  ];
+  
+  export default data;
+  
